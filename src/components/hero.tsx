bc7a7865dd90cebf07d2f2f07456b51f5ef232d1
Urooj-Memon <danishmemon2016@gmail.com>
2024-10-03 22:37:58 +0500

    projects

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
         <Image className='z-8 h-screen w-full object-cover object-center'src={coffeImage}alt={'COFFEE'}></Image>
          <div className="absolute inset-0 flex flex-col justify-center items-end pr-10 bg-black bg-opacity-30 text-white">
           <h1 className="text-5xl font-bold mb-4 text-right">Welcome To Coffee Shop</h1>
-          <p className="text-xl mb-8 text-right py-3 w-1/2">Start your day with the energy and warmth of our expertly brewed coffee. It's more than just a drink—it's an experience.</p>
+          <p className="text-xl mb-8 text-right py-3 w-1/2">Start your day with the energy and warmth of our expertly brewed coffee. Its more than just a drink—its an experience.</p>
           <button className="bg-yellow-600 text-white py-2 px-6 rounded-lg hover:bg-yellow-500 mx-60">Order Now</button>
         </div>
       </main> 
