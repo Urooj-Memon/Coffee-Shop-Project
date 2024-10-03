e79623fcdc462dcc59d7bf1f7bf4bd4607da29e9
Urooj-Memon <danishmemon2016@gmail.com>
2024-10-03 22:23:00 +0500

    projects

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
     <>
     <main className='relative'>
 
-        <Image className='z-8 h-screen w-full object-cover object-center'  src={coffeImage} alt={'COFFEE'}></Image>
+        <Image className='z-8 h-screen w-full object-cover object-center'src={coffeImage}alt={'COFFEE'}></Image>
          <div className="absolute inset-0 flex flex-col justify-center items-end pr-10 bg-black bg-opacity-30 text-white">
           <h1 className="text-5xl font-bold mb-4 text-right">Welcome To Coffee Shop</h1>
           <p className="text-xl mb-8 text-right py-3 w-1/2">Start your day with the energy and warmth of our expertly brewed coffee. It's more than just a drink—it's an experience.</p>
